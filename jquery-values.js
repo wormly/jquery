d0b6efeb63992a46b1ebca00432823620c69b5d7
Rodion Vynnychenko <roddiku@gmail.com>
2014-10-09 20:31:56 +0300

    added jquery-values radio support

--- jquery-values.js
+++ jquery-values.js
@@ -1,35 +1,35 @@
- /* jQuery.values: get or set all of the name/value pairs from child input controls	 
-	* @argument data {array} If included, will populate all child controls.
-	* @returns element if data was provided, or array of values if not
-	* Thanks to http://stackoverflow.com/questions/1489486/jquery-plugin-to-serialize-a-form-and-also-restore-populate-the-form
-*/
+/* jQuery.values: get or set all of the name/value pairs from child input controls	 
+ * @argument data {array} If included, will populate all child controls.
+ * @returns element if data was provided, or array of values if not
+ * Thanks to http://stackoverflow.com/questions/1489486/jquery-plugin-to-serialize-a-form-and-also-restore-populate-the-form
+ */
 
-$.fn.values = function(data) {
+$.fn.values = function (data) {
 	var els = this.is(':input') ? this : this.find(':input').get();
 
-	if(arguments.length === 0) {
+	if (arguments.length === 0) {
 		// return all data
 		data = {};
 
-		$.each(els, function() {
-			if (this.name && !this.disabled && (
-				/select|textarea/i.test(this.nodeName)
-				|| /text|hidden|password|checkbox|range/i.test(this.type))) {
-                    				if(data[this.name] == undefined) {
-                    					data[this.name] = [];
-                    				}
-                    				val = $(this).val()
+		$.each(els, function () {
+			var supportedType = (/select|textarea/i.test(this.nodeName) || /text|hidden|password|checkbox|range|radio/i.test(this.type));
 
-                    				if (/checkbox/i.test(this.type)) {
-						if (!this.checked) val = null;
-					}
+			if (this.name && !this.disabled && supportedType) {
+				if (data[this.name] == undefined) {
+					data[this.name] = [];
+				}
+				val = $(this).val()
 
-                    				data[this.name].push(val);
+				if (/checkbox/i.test(this.type)) {
+					if (!this.checked) val = null;
+				}
+
+				data[this.name].push(val);
 			}
 		});
-		
+
 		// Get rid of arrays for elements which are scalar values:
-		$.each(data, function(elem, values) {
+		$.each(data, function (elem, values) {
 			if (values.length == 1) {
 				data[elem] = values[0]
 			}
@@ -37,23 +37,23 @@ $.fn.values = function(data) {
 
 		return data;
 	} else {
-		$.each(els, function() {
+		$.each(els, function () {
 			if (this.name && data[this.name] !== undefined) {
 				var names = data[this.name];
 				var $this = $(this);
-				if(Object.prototype.toString.call(names) !== '[object Array]'){
+				if (Object.prototype.toString.call(names) !== '[object Array]') {
 					names = [names]; //backwards compat to old version of this code
 				}
-				if(this.type == 'checkbox' || this.type == 'radio') { 
+				if (this.type == 'checkbox' || this.type == 'radio') {
 					var val = $this.val();
 					var found = false;
-					for(var i = 0; i < names.length; i++){
-						if(names[i] == val){
+					for (var i = 0; i < names.length; i++) {
+						if (names[i] == val) {
 							found = true;
 							break;
 						}
 					}
-					$this.attr("checked", found);
+					$this.prop("checked", found);
 				} else {
 					$this.val(names[0]);
 				}
